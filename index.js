b0d1c288417be0d43dadb49a1a088106e1bb09f2
znabhi <chotu12584@gmail.com>
2024-05-21 10:12:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/abhi", (req, res) => {
+  res.send("Hi abhishek jain");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
